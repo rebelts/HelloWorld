826b991e8a428d5a98e2fe873bd2078317a86ab3
harris\fb70131 <FBoladeres@picis.com>
2023-08-29 01:24:12 +0200

    addred xml file

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
 Console.WriteLine("Hello, World!");
+
+// Load XML document
+XmlDocument xmlDoc = new XmlDocument();
+xmlDoc.Load("sample.xml"); // Replace with your XML file path
+
+// Get the root node
+XmlNode rootNode = xmlDoc.DocumentElement;
+
+// Print values of all child nodes
+foreach (XmlNode childNode in rootNode.ChildNodes)
+{
+    Console.WriteLine($"Tag Name: {childNode.Name}, Value: {childNode.InnerText}");
+}
+
+
